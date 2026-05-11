0b05a55bac615c4c2361bd19083d0f28fdf570ec
Mohammad Abdul-Shafi <mohamedsadik763@gmail.com>
2026-05-11 03:39:37 +0300

    chore: add pdf and printing dependencies for AI reports

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -15,6 +15,7 @@ import flutter_local_notifications
 import flutter_secure_storage_macos
 import google_sign_in_ios
 import path_provider_foundation
+import printing
 import shared_preferences_foundation
 import sqflite_darwin
 
@@ -29,6 +30,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
